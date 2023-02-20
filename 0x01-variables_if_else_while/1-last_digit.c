0ac3b2c1cfcacdeecc10c48f0cfb3f3a5cbc8b64
TishaLima <TishaLima>
2023-02-20 12:37:39 -0800

    Tisha's commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX ? 2;
+	a = n % 10;
+
+
+	if (
