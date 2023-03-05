50f89702fb8e8c25adafb33df9ddd8a905557e87
TishaLima <TishaLima>
2023-03-05 13:45:54 -0800

    if else

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-
 /**
- * main - prints of combination of single digits
- * Return: Always 0
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -17,7 +16,8 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
-
 	return (0);
 }
+
